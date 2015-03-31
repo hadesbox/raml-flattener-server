6cb9c280d6f83b0ec3bab12dc9c4938be9d92992
Luis Gonzalez <hadesbox@gmail.com>
2015-03-31 10:33:32 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var raml = require('raml-parser');
+
+var http = require('http');
+
+http.createServer(function(req, res) {
+
+    srcFile = 'http://apiraml.digitalservices.es' + req.url
+
+    console.log('will parse file http://apiraml.digitalservices.es' + req.url);
+
+    var flattener = require('flat-raml');
+
+    flattener.asString(srcFile).done(function(ramlstring) {
+
+        
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(ramlstring);
+
+    })
+
+}).listen(3000);
